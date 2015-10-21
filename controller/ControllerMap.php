b4ae252214adf7524542207e76fb39dcb21409a9
Ivan <cheremhovo1990@yandex.ru>
2015-10-21 09:36:22 +0900

    Закончил создание класса ControllerMap

--- controller/ControllerMap.php
+++ controller/ControllerMap.php
@@ -3,6 +3,8 @@ namespace controller;
 
 class ControllerMap {
 	private $viewMap = [];
+	private $classRootMap = [];
+	private $forwardMap = [];
 
 	function addView( $command = 'default', $status = 0, $view){
 		$command = strtolower($command);
@@ -13,6 +15,32 @@ class ControllerMap {
 		$command = strtolower($command);
 		if (isset($this->viewMap[$command][$status])){
 			return $this->viewMap[$command][$status];
+		}
+		return null;
+	}
+
+	function addClassRoot($command, $classAlias){
+		$command = strtolower($command);
+		$this->classRootMap[$command] = $classAlias;
+	}
+
+	function getClassRoot($command){
+		$command = strtolower($command);
+		if ($this->classRootMap[$command]){
+			return $this->classRootMap[$command];
+		}
+		return null;
+	}
+
+	function addForward($command, $status=0, $newCommand){
+		$command = strtolower($command);
+		$this->forwardMap[$command][$status] = $newCommand;
+	}
+
+	function getForward($command, $status){
+		$command = strtolower($command);
+		if (isset($this->forwardMap[$command][$status])){
+			return $this->forwardMap[$command][$status];
 		}
 		return null;
 	}
