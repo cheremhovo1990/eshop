1ed4dabbf09f85a5887dfdb5ddd688c540def7f5
Ivan <cheremhovo1990@yandex.ru>
2015-10-26 10:37:50 +0800

    Добвали session_start()

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?
+define ('BASE_URI', 'D:\openserver\domains\eshop.loc/');
+define ('BASE_URL', 'eshop.loc/');
+define ('PDFS_DIR', BASE_URI . 'pdfs/');
+session_start();
+
 spl_autoload_register(function($class){
-	require_once __DIR__ . '/' . $class . '.php';
+	require_once __DIR__ . DIRECTORY_SEPARATOR . $class . '.php';
 });
 
 controller\FrontController::run();
