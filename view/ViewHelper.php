3b8856d7d0a3431a540a2dfd9499b73af82d79be
Ivan <cheremhovo1990@yandex.ru>
2015-10-27 09:34:07 +0800

    добавил метод redirect_invalid_user() классу ViewHelper

--- view/ViewHelper.php
+++ view/ViewHelper.php
@@ -7,6 +7,16 @@ class ViewHelper {
     static function getRequest() {
         return \base\ApplicationRegistry::getRequest();
     }
+
+    public static function redirect_invalid_user($check = 'user_id', $destination = 'index.php', $protocol = 'http://')
+    {
+        // Check for the session item:
+        if (!isset($_SESSION[$check])) {
+            $url = $protocol . BASE_URL . $destination; // Define the URL.
+            header("Location: $url");
+            exit(); // Quit the script.
+        }
+    }
 }
 
 ?>
