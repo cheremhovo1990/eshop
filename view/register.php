90e8872d091fc4046dcbcea78764d0b953cc200f
Ivan <cheremhovo1990@yandex.ru>
2015-10-28 10:51:05 +0800

    создал страницу register

--- view/register.php
+++ view/register.php
@@ -24,7 +24,7 @@ $reg_errors = $request->getErrors();
 // Next block added in Chapter 4:
 // If it's a POST request, handle the login attempt:
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-	include('./includes/login.inc.php');
+	//include('./includes/login.inc.php');
 }
 
 // Include the header file:
@@ -32,7 +32,7 @@ require "includes/header.php";
 ?>
 	<h1>Register</h1>
 <p>Access to the site's content is available to registered users at a cost of $10.00 (US) per year. Use the form below to begin the registration process. <strong>Note: All fields are required.</strong> After completing this form, you'll be presented with the opportunity to securely pay for your yearly subscription via <a href="http://www.paypal.com">PayPal</a>.</p>
-<form action="register.php" method="post" accept-charset="utf-8">
+<form action="http://<?=BASE_URL?>?cmd=register" method="post" accept-charset="utf-8">
 <?php
 VH::create_form_input('first_name', 'text', 'First Name', $reg_errors);
 VH::create_form_input('last_name', 'text', 'Last Name', $reg_errors);
---
+++ view/register_CMD_OK.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ivan
+ * Date: 27.10.2015
+ * Time: 9:02
+ */
+use \view\ViewHelper as VH;
+
+$request = VH::getRequest();
+
+$title = $request->getTitle();
+$cats = $request->getArray('cats');
+$reg_errors = $request->getErrors();
+$e = trim($request->getVariable('email'), '\'');
+// To test the sidebars:
+//$_SESSION['user_id'] = 1;
+//$_SESSION['user_admin'] = true;
+//$_SESSION['user_not_expired'] = true;
+//$_SESSION=array();
+
+// Require the database connection:
+//require(MYSQL);
+
+// Next block added in Chapter 4:
+// If it's a POST request, handle the login attempt:
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+	//include('./includes/login.inc.php');
+}
+
+// Include the header file:
+require "includes/header.php";
+// Display a thanks message...
+
+// Original message from Chapter 4:
+// echo '<div class="alert alert-success"><h3>Thanks!</h3><p>Thank you for registering! You may now log in and access the site\'s content.</p></div>';
+
+// Updated message in Chapter 6:
+echo '<div class="alert alert-success"><h3>Thanks!</h3><p>Thank you for registering! To complete the process, please now click the button below so that you may pay for your site access via PayPal. The cost is $10 (US) per year. <strong>Note: When you complete your payment at PayPal, please click the button to return to this site.</strong></p></div>';
+
+// PayPal link added in Chapter 6:
+echo '<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
+	<input type="hidden" name="cmd" value="_s-xclick">
+	<input type="hidden" name="email" value="' . $e . '">
+	<input type="hidden" name="hosted_button_id" value="8YW8FZDELF296">
+	<input type="image" src="https://www.sandbox.paypal.com/en_US/i/btn/btn_subscribeCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+	<img alt="" border="0" src="https://www.sandbox.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+</form>
+';
+
+// Send a separate email?
+$body = "Thank you for registering at <whatever site>. Blah. Blah. Blah.\n\n";
+	mail($_POST['email'], 'Registration Confirmation', $body, 'From: admin@example.com');
+
+	// Finish the page:
+include "includes/footer.php";
