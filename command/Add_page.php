b4fb1079f8f6c39061b9b8e04f6cf0aa9da799fe
Ivan <cheremhovo1990@yandex.ru>
2015-10-30 10:29:51 +0800

    создал страницу add_page

---
+++ command/Add_page.php
@@ -0,0 +1,76 @@
+<?
+namespace command;
+
+class Add_page extends \command\Command{
+	function doExecute(\controller\Request $request){
+		$pdo = \PDO\ConnectPDO::instance();
+		$request->setTitle('Add a Site Content Page');
+		$sql = 'SELECT * FROM categories ORDER BY category';
+		$result = $pdo->query($sql);
+		$cats = $result->fetchAll(\PDO::FETCH_ASSOC);
+		$request->setArray('cats', $cats);
+		$add_page_errors = array();
+
+		if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+			// Check for a title:
+			if (!empty($_POST['title'])) {
+				$t = $pdo->quote(strip_tags($_POST['title']));
+			} else {
+				$add_page_errors['title'] = 'Please enter the title!';
+			}
+
+			// Check for a category:
+			if (filter_var($_POST['category'], FILTER_VALIDATE_INT, array('min_range' => 1))) {
+				$cat = $_POST['category'];
+			} else { // No category selected.
+				$add_page_errors['category'] = 'Please select a category!';
+			}
+
+			// Check for a description:
+			if (!empty($_POST['description'])) {
+				$d = $pdo->quote(strip_tags($_POST['description']));
+			} else {
+				$add_page_errors['description'] = 'Please enter the description!';
+			}
+
+			// Check for the content:
+			if (!empty($_POST['content'])) {
+				$allowed = '<div><p><span><br><a><img><h1><h2><h3><h4><ul><ol><li><blockquote>';
+				$c = $pdo->quote(strip_tags($_POST['content'], $allowed));
+			} else {
+				$add_page_errors['content'] = 'Please enter the content!';
+			}
+
+			if (empty($add_page_errors)) { // If everything's OK.
+
+				// Add the page to the database:
+				$q = "INSERT INTO pages (categories_id, title, description, content) VALUES ($cat, $t, $d, $c)";
+				$r = $pdo->exec($q);
+
+				if ($r === 1) { // If it ran OK.
+
+					// Print a message:
+					echo '<div class="alert alert-success"><h3>The page has been added!</h3></div>';
+
+					// Clear $_POST:
+					$_POST = array();
+
+					// Send an email to the administrator to let them know new content was added?
+
+				} else { // If it did not run OK.
+					trigger_error('The page could not be added due to a system error. We apologize for any inconvenience.');
+				}
+
+			} // End of $add_page_errors IF.
+
+		} // End of the main form submission conditional.
+
+		$q = "SELECT id, category FROM categories ORDER BY category ASC";
+		$r = $pdo->query($q);
+		$cats2 = $r->fetchAll(\PDO::FETCH_NUM);
+		$request->setArray('cats2', $cats2);
+		$request->setErrors($add_page_errors);
+		return self::statuses('CMD_DEFAULT');
+	}
+}
---
+++ view/add_page.php
@@ -0,0 +1,118 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ivan
+ * Date: 27.10.2015
+ * Time: 9:02
+ */
+use \view\ViewHelper as VH;
+
+$request = VH::getRequest();
+
+$title = $request->getTitle();
+$cats = $request->getArray('cats');
+$add_page_errors = $request->getErrors();
+$cats2 = $request->getArray('cats2');
+// To test the sidebars:
+//$_SESSION['user_id'] = 1;
+//$_SESSION['user_admin'] = true;
+//$_SESSION['user_not_expired'] = true;
+//$_SESSION=array();
+
+// Require the database connection:
+//require(MYSQL);
+
+// Next block added in Chapter 4:
+// If it's a POST request, handle the login attempt:
+
+
+// Include the header file:
+require "includes/header.php";
+
+/* PAGE CONTENT STARTS HERE! */
+?>
+	<h1>Add a Site Content Page</h1>
+	<form action="?cmd=add_page" method="post" accept-charset="utf-8">
+
+		<fieldset><legend>Fill out the form to add a page of content:</legend>
+			<div class="form-group">
+				<label for="status" class="control-label">Status</label>
+				<select name="status" class="form-control"><option value="draft">Draft</option>
+					<option value="live">Live</option>
+				</select></div>
+
+			<?php
+			VH::create_form_input('title', 'text', 'Title', $add_page_errors);
+
+			// Add the category drop down menu:
+			echo '<div class="form-group';
+			if (array_key_exists('category', $add_page_errors)) echo ' has-error';
+
+			/*
+			echo '"><label for="category" class="control-label">Category</label>
+			<select name="category" class="form-control">
+			<option>Select One</option>';
+			*/
+
+			// Bonus material!
+			// Added in Chapter 12.
+			// Allow for multiple categories:
+			echo '"><label for="category" class="control-label">Category</label>
+<select name="category" class="form-control" multiple size="5">';
+
+			// Retrieve all the categories and add to the pull-down menu:
+			//$q = "SELECT id, category FROM categories ORDER BY category ASC";
+			//$r = mysqli_query($dbc, $q);
+/*			while ($row = mysqli_fetch_array($r, MYSQLI_NUM)) {
+				echo "<option value=\"$row[0]\"";
+				// Check for stickyness:
+				if (isset($_POST['category']) && ($_POST['category'] == $row[0]) ) echo ' selected="selected"';
+				echo ">$row[1]</option>\n";
+			}*/
+
+			foreach ($cats2 as $row) {
+				echo "<option value=\"$row[0]\"";
+				if (isset($_POST['category']) && ($_POST['category'] == $row[0]) ) echo ' selected="selected"';
+				echo ">$row[1]</option>\n";
+			}
+
+
+			echo '</select>';
+			if (array_key_exists('category', $add_page_errors)) echo '<span class="help-block">' . $add_page_errors['category'] . '</span>';
+			echo '</div>';
+
+			VH::create_form_input('description', 'textarea', 'Description', $add_page_errors);
+			VH::create_form_input('content', 'textarea', 'Content', $add_page_errors);
+			?>
+
+			<input type="submit" name="submit_button" value="Add This Page" id="submit_button" class="btn btn-default" />
+
+		</fieldset>
+
+	</form>
+
+	<script type="text/javascript" src="js/tinymce/tinymce.min.js"></script>
+	<script type="text/javascript">
+		tinyMCE.init({
+			// General options
+			selector : "#content",
+			width : 800,
+			height : 400,
+			browser_spellcheck : true,
+
+			plugins: "paste,searchreplace,fullscreen,hr,link,anchor,image,charmap,media,autoresize,autosave,contextmenu,wordcount",
+
+			toolbar1: "cut,copy,paste,|,undo,redo,removeformat,|hr,|,link,unlink,anchor,image,|,charmap,media,|,search,replace,|,fullscreen",
+			toolbar2:	"bold,italic,underline,strikethrough,|,alignleft,aligncenter,alignright,alignjustify,|,formatselect,|,bullist,numlist,|,outdent,indent,blockquote,",
+
+			// Example content CSS (should be your site CSS)
+			content_css : "/ex1/html/css/bootstrap.min.css",
+
+		});
+	</script>
+	<!-- /TinyMCE -->
+<?php /* PAGE CONTENT ENDS HERE! */
+
+// Include the footer file to complete the template:
+
+include "includes/footer.php";
